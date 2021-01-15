64a40faa284cbd92b9fd4fa1dc3794129dc19f6b
Song-YunMin <thd930308@naver.com>
2021-01-15 16:50:44 +0900

    [ADD] : Object 추가

--- JS-OOP-Example/object.js
+++ JS-OOP-Example/object.js
@@ -3,7 +3,12 @@ let memberObject = {
     developer: 'graphittie',
     designer: 'leezhce'
 }
+// 멤버 수정
 memberObject.designer = 'leezche';
+
 console.log(memberObject.designer);
 console.log(memberObject['designer']);
 
+// 멤버 삭제
+delete memberObject.manager;
+console.log(memberObject.manager);
