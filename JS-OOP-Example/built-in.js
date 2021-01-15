d34629f1e7043eb03cc5221f538a0b1b6fe53965
YunMin-Song <thd930308@naver.com>
2021-01-16 00:18:07 +0900

    [ADD] : Add Built-in.js

--- JS-OOP-Example/built-in.js
+++ JS-OOP-Example/built-in.js
@@ -1,3 +1,19 @@
 console.log(Math.PI);
-console.log(Math.random());
+console.log(Math.random());         // method
 console.log(Math.floor(3.9));
+console.log("------------------------------");
+
+
+let MyMath = {
+    PI:Math.PI,
+    random: function(){
+        return Math.random();
+    },
+    floor:function (val) {
+        return Math.floor(val);
+    }
+}
+
+console.log(MyMath.PI);
+console.log(MyMath.random());
+console.log(MyMath.floor(3.9));
