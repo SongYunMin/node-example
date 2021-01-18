493276f24f67c2c44280f515d56cf17b4f714010
Song-YunMin <thd930308@naver.com>
2021-01-18 11:54:47 +0900

    [UPDATE] : 객체 상속방법 업데이트

--- JS-OOP-Example/prototype-inheritance.js
+++ JS-OOP-Example/prototype-inheritance.js
@@ -1,12 +1,42 @@
-let superObj = {superVal:'super'};
-// let subObj = {subVal : 'sub'};
-// subObj.__proto__ = superObj;        // subObj가 superObj의 자식임을 선언
-let subObj = Object.create(superObj);       // __proto__ 보다 object.create가 더 좋음
-subObj.subVal='sub';
-debugger;                               // 브라우저에서 중단점 역할을 할 수 있음
-console.log('subObj.subVal => ', subObj.subVal);
-console.log('subObj.superVal => ', subObj.superVal);
+// let superObj = {superVal: 'super'};
+// let subObj = Object.create(superObj);       // __proto__ 보다 object.create가 더 좋음
+// subObj.subVal = 'sub';
+// debugger;                               // 브라우저에서 중단점 역할을 할 수 있음
+// console.log('subObj.subVal => ', subObj.subVal);
+// console.log('subObj.superVal => ', subObj.superVal);
+//
+// // 변경을 시도하지만 아래 코드는 객체를 바꾼게 아님
+// subObj.superVal = 'sub';
+// console.log('superObj.superVal => ', superObj.superVal);  // superObj.superVal => super
 
-// 변경을 시도하지만 아래 코드는 객체를 바꾼게 아님
-subObj.superVal = 'sub';
-console.log('superObj.superVal => ', superObj.superVal);  // superObj.superVal => super
+// kim 객체 생성
+kim = {
+    name: 'kim',
+    first: 10, second: 20,
+    sum: function () {
+        return this.first + this.second
+    }
+}
+
+// Object.create() 를 이용한 상속
+let lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 10;
+lee.avg = function () {
+    return (this.first + this.second) / 2;
+}
+
+// // __proto__ 를 이용한 상속
+// lee = {
+//     name:'lee',
+//     first:10,second:10,
+//     avg: function(){
+//         return (this.first + this.second) / 2;
+//     }
+// }
+// lee.__proto__ = kim;
+
+console.log('kim.sum()', kim.sum());
+console.log('lee.sum()', lee.sum());
+console.log('lee.avg()', lee.avg());
