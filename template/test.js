8652bcc57cc13a0b75137c9ad8c86495159db199
YunMin-Song <thd930308@naver.com>
2021-01-21 23:54:53 +0900

    [ADD] : Templete Test

--- template/test.js
+++ template/test.js
@@ -1,6 +1,24 @@
 class Desktop{
-    constructor() {
+    constructor(color, desktop) {
+        this.color = color;
+        this.desktop = desktop;
+        this.changeDesktop();
+        this.supportsTemplate();
+    }
 
+    supportsTemplate(){
+        return 'content' in document.createElement('template');
+    }
+
+    changeDesktop(){
+        const desktop = document.querySelector("#template-desktop");
+        desktop.content.querySelector(".desktop");
+        let clone = document.importNode(desktop.content, true);
+        document.body.appendChild(clone);
+
+        const desktopColor = document.querySelector(".desktop");
+        console.log(desktopColor);
+        desktopColor.style.backgroundColor = this.color;
     }
 }
 
